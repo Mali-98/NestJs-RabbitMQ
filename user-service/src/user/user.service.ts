aa8acb2bc6f32b11baa441db41723220b237b635
MostafaMahmoud996 <mali@arconme.com>
2025-04-23 13:55:03 +0200

    create the user dto, add the microservice in the service file and create the createUser method

--- user-service/src/user/user.service.ts
+++ user-service/src/user/user.service.ts
@@ -1,11 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  private client: ClientProxy;
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {
+    this.client = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'user_queue',
+        queueOptions: { durable: false },
+      },
+    });
+  }
+  async createUser(createUserDto: CreateUserDto) {
+    const user = this.userRepo.create(createUserDto); // Use DTO here
+    const savedUser = await this.userRepo.save(user);
+    this.client.emit('user_created', savedUser); // Send event to RabbitMQ
+    return savedUser;
   }
 
   findAll() {
