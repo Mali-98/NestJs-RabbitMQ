ef8b70368efb8e328bce83012b7157421318fa30
MostafaMahmoud996 <mali@arconme.com>
2025-04-23 13:49:27 +0200

    Add the postgres database credentials to the app.module file

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5431,
+      username: 'postgres_rabbitMQ',
+      password: 'postgres_rabbitMQ',
+      database: 'postgres_rabbitMQ',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
